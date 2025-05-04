756e5aa8808a319ceb47d49ad09c3b4b1b78bb1b
gabrielvedova <bielvedova@gmail.com>
2025-05-04 17:48:55 -0300

    fix(timer): Update Intervals component to adjust timer settings and improve interval logic

--- doropomo/src/components/timer/Intervals.js
+++ doropomo/src/components/timer/Intervals.js
@@ -8,28 +8,36 @@ export default () => {
   const study = 1500; // 25 minutos
   const shortBreak = 300; // 5 minutos
   const longBreak = 900; // 15 minutos
-  const totalIntervals = Math.floor(totalTime / (study + shortBreak)) * 2; // Total de intervalos de estudo e pausa
+  const totalIntervals = Math.floor(
+    (cycleTime / (study + shortBreak)) * 2 * (totalTime / cycleTime)
+  ); // Total de intervalos de estudo e pausa
 
   const [currentInterval, setCurrentInterval] = useState(0); // Controle do intervalo atual
   const [currentType, setCurrentType] = useState("study"); // Tipo atual (estudo ou pausa)
 
   const [timerDuration, setTimerDuration] = useState(study); // Duração do temporizador atual
+  const [qntdIntervals, setQntdIntervals] = useState(0); // Contador de intervalos
 
   const handleTimerEnd = () => {
     if (currentInterval < totalIntervals - 1) {
       const nextInterval = currentInterval + 1;
       setCurrentInterval(nextInterval);
+      console.log("Total Intervalos:", totalIntervals);
+      console.log("Intervalo atual:", nextInterval);
+      console.log("Intervalos: ", qntdIntervals);
 
       // Alterna entre estudo, pausa curta e pausa longa
-      if (nextInterval % 7 === 0 && nextInterval !== 0) {
-        setCurrentType("longBreak");
-        setTimerDuration(longBreak); // Atualiza a duração para pausa longa
-      } else if (nextInterval % 2 === 0) {
+      if (nextInterval % 2 === 0) {
         setCurrentType("study");
         setTimerDuration(study); // Atualiza a duração para estudo
-      } else {
+      } else if (nextInterval % 2 !== 0 && qntdIntervals < 3) {
         setCurrentType("shortBreak");
         setTimerDuration(shortBreak); // Atualiza a duração para pausa curta
+        setQntdIntervals(qntdIntervals + 1); // Incrementa o contador de intervalos
+      } else if (nextInterval % 2 !== 0 && qntdIntervals >= 3) {
+        setCurrentType("longBreak");
+        setTimerDuration(longBreak); // Atualiza a duração para pausa longa
+        setQntdIntervals(0); // Reinicia o contador de intervalos
       }
     } else {
       // Finaliza os intervalos
