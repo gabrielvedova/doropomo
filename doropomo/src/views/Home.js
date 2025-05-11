74641c12535e746905c479077b1da0af66d7b05a
gabrielvedova <bielvedova@gmail.com>
2025-05-11 14:13:20 -0300

    feat(colors): Add colors.json for consistent color management across components

---
+++ doropomo/src/views/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Text, SafeAreaView, View, StyleSheet } from "react-native";
+import Tasks from "./Tasks";
+import Timer from "./Timer";
+import colors from "../colors.json";
+
+export default (props) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={{ color: colors.text }}>Media Player</Text>
+        <Text>Home</Text>
+      </View>
+      <View style={styles.pomodoroContainer}>
+        <Tasks />
+        <Timer />
+      </View>
+      <View>
+        <Text>Relatory</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.background,
+  },
+  pomodoroContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    height: "30%",
+    padding: 20,
+  },
+});
