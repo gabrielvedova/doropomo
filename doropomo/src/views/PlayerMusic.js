7312f465d6e1a1ae815aa59aa12443f718cc66a6
Gabriel Vedova <126964167+gabrielvedova@users.noreply.github.com>
2025-05-14 18:13:50 +0000

    feat(PlayerMusic): remove PlayerMusic component and associated getMusic logic

--- doropomo/src/views/PlayerMusic.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-import GetMusic from "../components/playerMusic/getMusic";
-
-export default (props) => {
-  return <GetMusic />;
-};
